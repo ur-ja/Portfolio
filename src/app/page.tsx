38c44e1980b3a2bff4526dd4d02cdea0c6164106
Urja Arora <urjaarora5@gmail.com>
2023-10-20 01:24:45 +1100

    make responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
+'use client';
+import React, { useEffect } from 'react';
 import DraggableComponent from '../components/DraggableComponent';
 import NavBar from '../components/NavBar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
+  const notify = () => toast('Try to drag and drop the elements!');
+
+  useEffect(() => {
+    // Call the notify function when the component mounts
+    notify();
+  }, []);
+
   return (
     <div className='pt-10 mb-10'>
       <NavBar />
-      <div className='mx-36 mt-12'>
+      <div className='lg:mx-36 mt-12'>
         <DraggableComponent />
+      </div>
+      <div>
+        <button onClick={notify}></button>
+        <ToastContainer />
       </div>
       <div className='flex justify-center items-center'>
         <p className='text-sm text-gray-300 font-extralight'>designed by nev</p>
