42d4196aaa15b0d9511c832b54bdbc01dad53c92
Urja Arora <urjaarora5@gmail.com>
2024-01-09 00:20:55 +0530

    feat(resume): create link to resume

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: '/resume.pdf',
+        permanent: true,
+      },
+    ];
+  },
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,11 @@ import Menu from './Menu';
 
 export default function NavBar() {
   return (
-    // TODO: Set nav location for nav screen
     <div className='flex flex-row justify-center lg:justify-between md:justify-between px-10'>
       <img src='/logo.svg' alt='Company Logo' className='h-auto w-auto' />
       {/* <Menu /> */}
-      <a
-        href='mailto:urjaarora048@gmail.com'
-        className='hidden md:inline-block'
-      >
-        Contact
+      <a className='hidden md:inline-block' href='/resume' target="_blank" rel="noopener">
+        Resume
       </a>
     </div>
   );
