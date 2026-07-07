4f71d279a67235e468baceb0d07592d048ad47fa
Urja Arora <urjaarora5@gmail.com>
2026-07-07 22:33:19 +0530

    Update resume link in next.config.js to point to Google Drive and set permanent redirect to false

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ module.exports = {
     return [
       {
         source: '/resume',
-        destination: '/resume.pdf',
-        permanent: true,
+        destination:
+          'https://drive.google.com/file/d/1hDp2_S6QPZ6hr-HJfR7pDflmKKpAlyHi/view?usp=sharing',
+        permanent: false,
       },
     ];
   },
